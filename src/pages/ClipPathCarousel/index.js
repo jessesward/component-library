626a03e1ace964eddfbfc2e3f08d4b21f971de58
Jesse Ward <jesseward@jessedev.local>
2022-07-13 09:13:55 -0700

    add components, update descriptions

--- src/pages/ClipPathCarousel/index.js
+++ src/pages/ClipPathCarousel/index.js
@@ -9,110 +9,65 @@ export default function ClipCarousel() {
 
   useEffect(() => {
     carousel.current = ClipPathCarousel(carouselElem.current)
-    carousel.current.init()
-    // if (!document.querySelector(".grid-wrapper")) {
-    //   carousel.current.init()
-    // }
+    if (!document.querySelector(".slide-dot")) {
+      carousel.current.init()
+    }
   }, [])
+
   return (
     <div className="page">
       <div ref={carouselElem} className="hero-slider slider-track">
-        <div
-          className="
-            slide
-            absolute-full
-            site-padding-x
-            flex
-            items-end
-            desktop:items-center
-          "
-        >
-          {/* <img
+        <div className="slide absolute-full">
+          <img
             className="absolute-full object-cover"
-            :src="item.slideImage.url"
+            src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
-          /> */}
-          <div className="slide-content relative">
+          />
+          <div className="slide-content">
             <h2 className="slide-title">Slide Title 1</h2>
             <div className="description">Slide description</div>
           </div>
         </div>
-        <div
-          className="
-            slide
-            absolute-full
-            site-padding-x
-            flex
-            items-end
-            desktop:items-center
-          "
-        >
-          {/* <img
+        <div className="slide absolute-full">
+          <img
             className="absolute-full object-cover"
-            :src="item.slideImage.url"
+            src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
             alt=""
-          /> */}
-          <div className="slide-content relative">
+          />
+          <div className="slide-content">
             <h2 className="slide-title">Slide Title 1</h2>
             <div className="description">Slide description</div>
           </div>
         </div>
-        <div
-          className="
-            slide
-            absolute-full
-            site-padding-x
-            flex
-            items-end
-            desktop:items-center
-          "
-        >
-          {/* <img
+        <div className="slide absolute-full">
+          <img
             className="absolute-full object-cover"
-            :src="item.slideImage.url"
+            src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
-          /> */}
-          <div className="slide-content relative">
+          />
+          <div className="slide-content">
             <h2 className="slide-title">Slide Title 3</h2>
             <div className="description">Slide description</div>
           </div>
         </div>
-        <div
-          className="
-            slide
-            absolute-full
-            site-padding-x
-            flex
-            items-end
-            desktop:items-center
-          "
-        >
-          {/* <img
+        <div className="slide absolute-full">
+          <img
             className="absolute-full object-cover"
-            :src="item.slideImage.url"
+            src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
-          /> */}
-          <div className="slide-content relative">
+          />
+          <div className="slide-content">
             <h2 className="slide-title">Slide Title 4</h2>
             <div className="description">Slide description</div>
           </div>
         </div>
-        <div
-          className="
-            slide
-            absolute-full
-            site-padding-x
-            flex
-            items-end
-            desktop:items-center
-          "
-        >
-          {/* <img
+        <div className="slide absolute-full">
+          <img
             className="absolute-full object-cover"
-            :src="item.slideImage.url"
+            src="https://images.unsplash.com/photo-1486870591958-9b9d0d1dda99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
-          /> */}
-          <div className="slide-content relative">
+          />
+          <div className="slide-content">
             <h2 className="slide-title">Slide Title 5</h2>
             <div className="description">Slide description</div>
           </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,40 @@
 import * as React from "react"
 import { Routes, Route, Link } from "react-router-dom"
+import "./home.scss"
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <h1>Library</h1>
+      <p>
+        Even though this is a React app, the individual components are not React
+        specific.
+      </p>
+      <p>
+        For the most part, each component/page is standalone, but they do share
+        scripts from the utils directory. The utils directory is also where you
+        can find helpful things like functions to calculate the distance between
+        two points.
+      </p>
+      <ul>
+        <li>
+          <Link to="/infinite-grid-carousel">
+            <h2>Infinite Grid Carousel</h2>
+          </Link>
+          <p>Autoplay, and mouse interactions on hover.</p>
+          <p>
+            Dependencies: GSAP (but only for the internal clock. could use
+            request animation frame instead.)
+          </p>
+        </li>
+        <li>
+          <Link to="/clip-path-carousel">
+            <h2>Clip Path Carousel</h2>
+          </Link>
+          <p>Swipe transition between slides using clip path.</p>
+          <p>Dependencies: GSAP</p>
+        </li>
+      </ul>
     </div>
   )
 }
